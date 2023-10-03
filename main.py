058bace5c1111bd3dff1f61229a4d282f39666b3
randombanana9 <jerzy.sosnowski009@gmail.com>
2023-10-03 10:41:14 +0100

    Update main.py
    Added a subroutine to draw the board

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame, time, random
 
 pygame.init()
 
-screen = pygame.display.set_mode((1200,675))
+screen = pygame.display.set_mode((1200,680))
 pygame.display.set_caption("Dastan")
 
 BLACK = (0,0,0)
@@ -17,15 +17,31 @@ play = True
 
 ticker = 0
 
+def drawBoard():
+    for i in range(6):
+        for j in range(6):
+            if i%2:
+                if j%2:
+                    pygame.draw.rect(screen, WHITE, ((50 + i*100, 40 + j*100), (100, 100)))
+                else:
+                    pygame.draw.rect(screen, GREEN, ((50 + i*100, 40 + j*100), (100, 100)))
+            else:
+                if j%2:
+                    pygame.draw.rect(screen, GREEN, ((50 + i*100, 40 + j*100), (100, 100)))
+                else:
+                    pygame.draw.rect(screen, WHITE, ((50 + i*100, 40 + j*100), (100, 100)))
+
 def draw(color, x, y):
     pygame.draw.circle(screen, color, (x, y), 40, 0)
 
 while play:
-    screen.fill(WHITE)
+    screen.fill(BLACK)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             play = False
+
+    drawBoard()
     
     pygame.display.update()
 
