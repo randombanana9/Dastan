a878f7900da466a25c39f97933d5bc4b3d8d16f0
randombanana9 <jerzy.sosnowski009@gmail.com>
2023-10-06 11:22:50 +0100

    Update main.py
    Changed the colours to make the aesthetics more appealing

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ pygame.init()
 screen = pygame.display.set_mode((1200,680))
 pygame.display.set_caption("Dastan")
 
-BLACK = (0,0,0)
+BACKGROUND = (40,40,40)
 RED = (255,0,0)
 YELLOW = (255,255,0)
-GREEN = (0,255,0)
-WHITE = (255,255,255)
+BOARD1 = (161, 116, 79)
+BOARD2 = (222, 221, 170)
+WHITE = (255, 255, 255)
 
 clock = pygame.time.Clock()
 
@@ -18,24 +19,25 @@ play = True
 ticker = 0
 
 def drawBoard():
+    pygame.draw.rect(screen, WHITE, ((49, 39), (602, 602)))
     for i in range(6):
         for j in range(6):
             if i%2:
                 if j%2:
-                    pygame.draw.rect(screen, WHITE, ((50 + i*100, 40 + j*100), (100, 100)))
+                    pygame.draw.rect(screen, BOARD2, ((50 + i*100, 40 + j*100), (100, 100)))
                 else:
-                    pygame.draw.rect(screen, GREEN, ((50 + i*100, 40 + j*100), (100, 100)))
+                    pygame.draw.rect(screen, BOARD1, ((50 + i*100, 40 + j*100), (100, 100)))
             else:
                 if j%2:
-                    pygame.draw.rect(screen, GREEN, ((50 + i*100, 40 + j*100), (100, 100)))
+                    pygame.draw.rect(screen, BOARD1, ((50 + i*100, 40 + j*100), (100, 100)))
                 else:
-                    pygame.draw.rect(screen, WHITE, ((50 + i*100, 40 + j*100), (100, 100)))
+                    pygame.draw.rect(screen, BOARD2, ((50 + i*100, 40 + j*100), (100, 100)))
 
 def draw(color, x, y):
     pygame.draw.circle(screen, color, (x, y), 40, 0)
 
 while play:
-    screen.fill(BLACK)
+    screen.fill(BACKGROUND)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
