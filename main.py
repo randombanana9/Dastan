c21a355c9d267ce0d8a3e2d7c7c4b0a941d06c58
Adam Wilson <2005adamw@gmail.com>
2023-10-06 16:08:15 +0100

    Improved code to draw board

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ WHITE = (255, 255, 255)
 BOARD_THEMES = [((140, 51, 19), (230, 166, 68)),
                 ((181, 136, 99), (240, 217, 181)),
                 ((116, 148, 84), (236, 236, 212))]
-BOARD1, BOARD2 = BOARD_THEMES[2]
+BOARD1, BOARD2 = BOARD_THEMES[1]
 
 clock = pygame.time.Clock()
 
@@ -25,16 +25,10 @@ def drawBoard():
     pygame.draw.rect(screen, WHITE, ((49, 39), (602, 602)))
     for i in range(6):
         for j in range(6):
-            if i%2:
-                if j%2:
-                    pygame.draw.rect(screen, BOARD2, ((50 + i*100, 40 + j*100), (100, 100)))
-                else:
-                    pygame.draw.rect(screen, BOARD1, ((50 + i*100, 40 + j*100), (100, 100)))
+            if (i + j)%2:
+                pygame.draw.rect(screen, BOARD1, ((50 + i*100, 40 + j*100), (100, 100)))
             else:
-                if j%2:
-                    pygame.draw.rect(screen, BOARD1, ((50 + i*100, 40 + j*100), (100, 100)))
-                else:
-                    pygame.draw.rect(screen, BOARD2, ((50 + i*100, 40 + j*100), (100, 100)))
+                pygame.draw.rect(screen, BOARD2, ((50 + i*100, 40 + j*100), (100, 100)))
 
 def draw(color, x, y):
     pygame.draw.circle(screen, color, (x, y), 40, 0)
