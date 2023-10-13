8d78aa02324b4413ec07cc1b12496802b4dd1d33
Adam Wilson <2005adamw@gmail.com>
2023-10-13 12:47:11 +0100

    Added draw function to QueueButton

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
 import pygame, time, random
 
+pygame.font.init() # you have to call this at the start, 
+                   # if you want to use this module.
+
 BACKGROUND = (40,40,40)
 RED = (255,0,0)
 YELLOW = (255,255,0)
@@ -11,15 +14,24 @@ FA = (200, 25, 25)
 CU = (190, 5, 190)
 JA = (255, 255, 0)
 
-moveColors = {"Ryott": RY, "Chowkidar": CH, "Faujdar": FA, "Cuirassier": CU, "Jazair": JA}
+moveColours = {"Ryott": RY, "Chowkidar": CH, "Faujdar": FA, "Cuirassier": CU, "Jazair": JA}
 
 class QueueButton:
-    def __init__(self, move, position = 0, cost = 0, size = (100,30)):
+    def __init__(self, move, cost = 0, size = (100,30)):
         self.move = move
-        self.position = position
         self.cost = cost
         self.size = size
-        self.color = moveColors[move]
+        self.text_colour = WHITE
+    
+    def draw(self, selected = False):
+        font = pygame.font.SysFont('Comic Sans MS', int(self.size[1]*0.8))
+
+        canvas = pygame.Surface(self.size)
+        canvas.fill(BACKGROUND)
+        pygame.draw.rect(canvas, moveColours[self.move], ((0, 0), self.size), border_radius = 5)
+        canvas.blit(font.render(self.move, True, self.text_colour), (0, 0))
+        return canvas
+
 
 
 class Game:
@@ -28,11 +40,15 @@ class Game:
               ((116, 148, 84), (236, 236, 212))]
 
     def __init__(self, size = (1200, 700)):
-        self.size = size
-        self.board_size = (size[1]*5/7)+(6-(size[1]*5/7)%6)+2  # this will give a square board of y size
+        self.size = size # Size of the game canvas
+        self.board_size = (size[1]*5/7)+(6-(size[1]*5/7)%6)+2  # this will give a square board of 5/7th of the y size (rounded up to the neares multiple of 6)
         self.queue_size = (size[0] - self.board_size, size[1])  # the remaining space left after board is taken
-        self.queue_pos = (size[1], 0) 
-        self.theme = 0
+        #The next line is prety simple so if you dont understand it its on you
+        self.queue_pos = ((2*(self.size[1] - self.board_size)//2.5-1) + self.board_size , (self.size[1] - self.board_size)//2-1 + self.board_size//3)  
+        self.theme = 0  # Current theme
+
+        ####temp#####
+        self.queue1 = [QueueButton(i) for i in moveColours.keys()]
 
     def draw(self) -> pygame.Surface:
         """Returns a surface contining the game drawn onto it"""
@@ -65,6 +81,8 @@ class Game:
         ######## TODO ##########
         canvas = pygame.Surface(self.queue_size)
         canvas.fill(BACKGROUND)
+        for index, button in enumerate(self.queue1):
+            canvas.blit(button.draw(), ((index)*110, 0))
         return canvas
 
     def handle_click(self, position):
